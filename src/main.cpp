3ed317120d9769038aab7601544a6d389f902032
cryptoAlgorithm <cryptoalgro@gmail.com>
2023-02-06 21:32:29 +0800

    feat: overhaul command handling with more sanity checks

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,21 @@
 #define ESC_2_PIN  8
 #define PSU_PIN    2
 
+
 Servo esc1;
 Servo esc2;
 
 // PSU macros
 #define PSU_ON() digitalWrite(PSU_PIN, 0); \
-                 Serial.println("PSU ON"); \
+                 Serial.println(F("PSU ON")); \
                  delay(500) // PSU delay to wait for PSU to turn on
 #define PSU_OFF() digitalWrite(PSU_PIN, 1); \
-                 Serial.println("PSU OFF")
+                 Serial.println(F("PSU OFF"))
 
-// Serial constants
-#define SPEED_PROMPT F("Set interval (1000-2000us): ")
+// Serial macros
+#define SERIAL_FLUSH() while (Serial.available()) Serial.read() // Clear the remaining data in the serial buffer
+#define SPEED_PROMPT() SERIAL_FLUSH(); Serial.println(F("Set interval (1000-2000us): "))
+
 
 /**
  * Initialises ESCs with correct startup sequence
@@ -28,7 +31,7 @@ Servo esc2;
  * This calibrates the max and min period durations.
 */
 void init_esc() {
-  Serial.println("ESC init start");
+  Serial.println(F("ESC init start"));
   esc1.attach(ESC_1_PIN);
   esc2.attach(ESC_2_PIN);
 
@@ -37,34 +40,26 @@ void init_esc() {
   esc2.writeMicroseconds(MAX_PERIOD);
 
   PSU_ON();
-  Serial.println(F("Calibrate HIGH"));
+  Serial.println(F("Calibrate max duty"));
   delay(2000); // ESC timeout
-  Serial.println(F("Calibrate LOW"));
+  Serial.println(F("Calibrate min duty"));
   esc1.writeMicroseconds(MIN_PERIOD);
   esc2.writeMicroseconds(MIN_PERIOD);
 
   Serial.println(F("ESC init done"));
 }
 
+
 void setup() {
+  // Init serial
+  Serial.begin(115200);
+  Serial.println(F("Start"));
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PSU_PIN, OUTPUT);
 
   // Switch off PSU initially
   PSU_OFF();
-
-  // Init serial
-  Serial.begin(115200);
-  Serial.println("Start!");
-
-  // Set OC1A pins to outputs
-  /*pinMode(9, OUTPUT); // OC1A = pin 9
-
-  // Write timer1 config registers
-  TCCR1A = _BV(COM1A0) | _BV(COM1B0) | _BV(WGM11) | _BV(WGM10);
-  TCCR1B = _BV(CS11) | _BV(CS10);
-  OCR1A = MAX_PERIOD;
-  OCR1B = MIN_PERIOD;*/
 
   // Init ESC with startup sequence
   init_esc();
@@ -72,37 +67,51 @@ void setup() {
   // This line signals to hosts that the initialisation sequence has completed
   Serial.println(F("Ready"));
 
-  Serial.print(SPEED_PROMPT);
+  SPEED_PROMPT();
 }
+
 
 void loop() {
   // Pull out all variables used in loop for better "memory management"
-  int32_t rawRead;
-  uint32_t interval;
-  char motorID;
-  while (Serial.available()) {
-    rawRead = Serial.parseInt();
-    if (rawRead < 0) {
-      switch (rawRead) {
-        case -1:
-          // Emergency PSU off
-          PSU_OFF();
-          break;
-        case -2:
-          init_esc();
-          break;
-      }
-      continue;
+  uint32_t rawRead, interval;
+  char act, next;
+
+  // Command format: <TBSO>[speed]
+  if (Serial.available()) {
+    act = Serial.read();
+
+    switch (act) {
+      case 'T': // Top ESC
+      case 'B': // Bottom ESC
+        break;
+      case 'S': // PSU [S]hutoff
+        PSU_OFF();
+        return;
+      case 'O': // PSU [O]n
+        // This will recalibrate range of ESCs
+        // Might not be neccessary, but we'll do it anyways
+        init_esc();
+        return;
+      default:
+        Serial.println(F("Invalid format, expected motor ID"));
+        return;
     }
+
+    // Make sure next character in buffer is a valid number
+    next = Serial.peek();
+    if (next < '0' || next > '9') { // Invalid char
+      Serial.println(F("Expected number after ESC ID"));
+      return;
+    }
+
+    rawRead = Serial.parseInt();
     interval = constrain(rawRead, MIN_PERIOD, MAX_PERIOD);
-    /* uint32_t interval = constrain(rawRead, MIN_PERIOD, MAX_PERIOD);
-    Serial.print("Set interval to "); Serial.print(interval); Serial.println("");
-    OCR1B = interval;*/
-    esc1.writeMicroseconds(interval);
-    esc2.writeMicroseconds(interval);
-    Serial.print("Set interval to "); Serial.print(interval); Serial.println("us");
-    // Clear the remaining data in the serial buffer
-    while (Serial.available()) Serial.read();
-    Serial.print(SPEED_PROMPT);
+    // Write us to ESC
+    act == 'T' ? esc1.writeMicroseconds(interval) : esc2.writeMicroseconds(interval);
+    // Print interval
+    Serial.print(F("Set interval of ESC <")); Serial.print(act); Serial.print(F("> to "));
+    Serial.print(interval); Serial.println(F("us"));
+
+    SPEED_PROMPT();
   }
 }
